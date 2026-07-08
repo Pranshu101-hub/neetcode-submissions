5560de935b33c5e5d0798e8eec222b6e9926a740
pranshu <prakashpranshu5@gmail.com>
2026-07-08 23:00:02 +0530

    Add: decode-ways - submission-4

---
+++ Data Structures & Algorithms/decode-ways/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo = {}
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            if i == len(s):
+                return 1
+            if s[i] == '0':
+                return 0
+            res = dfs(i+1)
+            if i<(len(s)-1) and int(s[i:i+2]) < 27:
+                res += dfs(i+2)
+            memo[i] = res
+            return res
+        return dfs(0)
